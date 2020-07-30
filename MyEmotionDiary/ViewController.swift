2e963cd19935960e6d2c81fe4d03589e603d790b
jmk9633 <jmk9633@naver.com>
2020-07-30 14:43:19 +0900

    Initial Commit

---
+++ MyEmotionDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyEmotionDiary
+//
+//  Created by 정문기 on 2020/07/30.
+//  Copyright © 2020 jmk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
